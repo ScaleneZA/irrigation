378a3b47d4efbbd0d74bc0c3ef26b9e316b7efe9
Luke <lukesmb@gmail.com>
2022-12-31 20:05:58 +0200

    Trying to make the scripts more runnable

--- scripts/run-all.py
+++ scripts/run-all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import RPi.GPIO as GPIO
 
 import config
--- scripts/start-station.py
+++ scripts/start-station.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-import sys
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import config
 
