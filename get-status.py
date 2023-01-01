716ade2655c9e32e81b23f2d16990794477cace2
Luke <lukesmb@gmail.com>
2023-01-01 15:35:00 +0200

    English status

--- get-status.py
+++ get-status.py
@@ -5,10 +5,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import RPi.GPIO as GPIO
 import config
 
+# Disable warnings because we rely on the output of this script.
+GPIO.setwarnings(False)
+
 # TODO: Extract common code. 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(config.commonPin, GPIO.OUT)
 
-print (GPIO.input(config.commonPin))
+status = "ON" if GPIO.input(config.commonPin) == GPIO.LOW else "OFF"
+
+print(status)
 
 GPIO.cleanup()
