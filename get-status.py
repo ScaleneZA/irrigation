de01a2429afed5c0b5b97fcb66f5cbee61465662
Luke <lukesmb@gmail.com>
2023-01-01 14:35:58 +0200

    Second fix

--- get-status.py
+++ get-status.py
@@ -5,5 +5,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import RPi.GPIO as GPIO
 import config
 
+# TODO: Extract common code. 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(config.commonPin, GPIO.OUT)
+
 print (GPIO.input(config.commonPin))
