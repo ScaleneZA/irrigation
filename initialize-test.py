b3575001b42d50551bca0cdd8618f9c46d44538b
Luke <lukesmb@gmail.com>
2023-01-04 17:40:23 +0200

    adding time

--- initialize-test.py
+++ initialize-test.py
@@ -9,15 +9,22 @@ GPIO.setmode(GPIO.BCM)
 try:
     GPIO.setup(config.commonPin, GPIO.OUT)
     GPIO.output(config.commonPin, GPIO.HIGH)
+    time.sleep(1)
     GPIO.output(config.commonPin, GPIO.LOW)
+    time.sleep(1)
     GPIO.output(config.commonPin, GPIO.HIGH)
+    time.sleep(1)
 
     # Initialize each GPIO pin
     for st in config.stations:
+        print(st["gpiopin"])
         GPIO.setup(st["gpiopin"], GPIO.OUT)
         GPIO.output(st["gpiopin"], GPIO.HIGH)
+        time.sleep(1)
         GPIO.output(st["gpiopin"], GPIO.LOW)
+        time.sleep(1)
         GPIO.output(st["gpiopin"], GPIO.HIGH)
+        time.sleep(1)
 
 finally:
     GPIO.cleanup()
