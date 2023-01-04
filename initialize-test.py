a5a999486c53fdd8f8813554c50a724232b23d7f
Luke <lukesmb@gmail.com>
2023-01-04 17:36:18 +0200

    Test Script for startup

---
+++ initialize-test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+import config
+
+GPIO.setmode(GPIO.BCM)
+
+try:
+    GPIO.output(config.commonPin, GPIO.HIGH)
+    GPIO.output(st["gpiopin"], GPIO.LOW)
+    GPIO.output(config.commonPin, GPIO.HIGH)
+
+    # Initialize each GPIO pin
+    for st in config.stations:
+        GPIO.setup(st["gpiopin"], GPIO.OUT)
+        GPIO.output(st["gpiopin"], GPIO.HIGH)
+        GPIO.output(st["gpiopin"], GPIO.LOW)
+        GPIO.output(st["gpiopin"], GPIO.HIGH)
+
+finally:
+    GPIO.cleanup()
